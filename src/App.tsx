eeae2cabc82a76a61ebd4472548a49b2b243c963
leila <leila.zitouni55@gmail.com>
2023-11-02 15:35:02 +0100

    Added React logic, uesRef hook and type script specifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Redirect, Route } from 'react-router-dom';
+import React, { useRef } from 'react';
 import {
   IonApp,
   IonButton,
@@ -39,53 +40,80 @@ import './theme/variables.css';
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle>BMI Calculator</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent className="ion-padding">
-      <IonGrid>
-        <IonRow>
-          <IonCol>
-            <IonItem>
-              <IonLabel position="floating">Your Height</IonLabel>
-              <IonInput></IonInput>
-            </IonItem>
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <IonCol>
-            <IonItem>
-              <IonLabel position="floating">Your Weight</IonLabel>
-              <IonInput></IonInput>
-            </IonItem>
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <IonCol className="ion-text-left">
-            <IonButton>
-              <IonIcon slot="start" icon={calculatorOutline} />
-              Calculate
-            </IonButton>
-          </IonCol>
-          <IonCol className="ion-text-right">
-            <IonButton>
-              <IonIcon slot="start" icon={refreshOutline} />
-              Reset
-            </IonButton>
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <IonCol></IonCol>
-        </IonRow>
-      </IonGrid>
-    </IonContent>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const weightInputRef = useRef<HTMLIonInputElement>(null); // see the comment at the end for useRef, a react hook
+  const heightInputRef = useRef<HTMLIonInputElement>(null); // here we create two ref objects, one for each input. We use the useRef Hook to create the ref objects. We also specify the type of the ref object. In this case, we want to access the value property of the ref object, so we specify the type as HTMLIonInputElement. This is a type that's provided by Ionic React. It's a type that represents an HTML input element. We also initialize the ref objects to null. This is because the ref objects are initially null. We'll assign the ref objects to the inputs later.
+
+  const calculateBMI = () => {
+    const enteredWeight = weightInputRef.current!.value;
+    const enteredHeight = heightInputRef.current!.value; // here we use the non-null assertion operator (!) to tell TypeScript that we know that the value is not null or undefined. This is a way to tell TypeScript that we know better than it does. It prevents TypeScript from throwing an error when we try to access the value property of the ref object. If the value is null or undefined, we'll get a runtime error, but we know that it won't be null or undefined because we've already checked for that.
+
+    if (!enteredHeight || !enteredWeight) {
+      return;
+    } // here we check if the enteredHeight or enteredWeight is falsy. If either of them is falsy, we return early from the function. This is a way to prevent the function from executing if the user hasn't entered a value for either of the inputs.
+
+    const bmi = +enteredWeight / (+enteredHeight * +enteredHeight); // here, the + operator converts the string values to numbers. This is a way to tell TypeScript that we know that the values are numbers. It prevents TypeScript from throwing an error when we try to perform a mathematical operation on the values.
+
+    console.log(bmi);
+  };
+
+  const resetInputs = () => {
+    weightInputRef.current!.value = '';
+    heightInputRef.current!.value = '';
+  };
+
+  return (
+    <IonApp>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>BMI Calculator</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonInput label="Your Height" ref={heightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonInput label="Your Weight" ref={weightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol className="ion-text-left">
+              <IonButton onClick={calculateBMI}>
+                <IonIcon slot="start" icon={calculatorOutline} />
+                Calculate
+              </IonButton>
+            </IonCol>
+            <IonCol className="ion-text-right">
+              <IonButton onClick={resetInputs}>
+                <IonIcon slot="start" icon={refreshOutline} />
+                Reset
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol></IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonApp>
+  );
+};
 
 export default App;
 
 // The IonIcon component is self-closing, so it doesn't need a closing tag.
+
+// useRef is a Hook in React that allows you to create a mutable ref object. Refs are a way to access the properties or the DOM (Document Object Model) elements of a React component directly. You can think of useRef as a way to "remember" values between renders without causing a re-render of the component.
+// Here's how you can use useRef:
+// 1. Import useRef: You've already imported useRef in your code with import React, { useRef } from 'react';.
+// 2. Create a Ref Object: const myRef = useRef(initialValue) or const weightInputRef = useRef<HTMLIonInputElement>(null); => initialValue (optional): You can provide an initial value if you want. This is particularly useful for initializing the ref to a DOM element, as in the case of useRef(null).
+// 3. Accessing the Current Value: You can access the current value of the ref using myRef.current. This is a property of the ref object that holds the current value. You can also assign new values to it.

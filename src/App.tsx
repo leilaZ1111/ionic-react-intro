f3536e0d6fdce03dea1ecaebd20c967158360153
leila <leila.zitouni55@gmail.com>
2023-11-03 13:45:58 +0100

    added useState for calculateBMI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Redirect, Route } from 'react-router-dom';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import {
   IonApp,
   IonButton,
+  IonCard,
+  IonCardContent,
   IonCol,
   IonContent,
   IonGrid,
@@ -41,6 +43,8 @@ import './theme/variables.css';
 setupIonicReact();
 
 const App: React.FC = () => {
+  const [calculatedBMI, setCalculatedBMI] = useState<number>();
+
   const weightInputRef = useRef<HTMLIonInputElement>(null); // see the comment at the end for useRef, a react hook
   const heightInputRef = useRef<HTMLIonInputElement>(null); // here we create two ref objects, one for each input. We use the useRef Hook to create the ref objects. We also specify the type of the ref object. In this case, we want to access the value property of the ref object, so we specify the type as HTMLIonInputElement. This is a type that's provided by Ionic React. It's a type that represents an HTML input element. We also initialize the ref objects to null. This is because the ref objects are initially null. We'll assign the ref objects to the inputs later.
 
@@ -53,8 +57,7 @@ const App: React.FC = () => {
     } // here we check if the enteredHeight or enteredWeight is falsy. If either of them is falsy, we return early from the function. This is a way to prevent the function from executing if the user hasn't entered a value for either of the inputs.
 
     const bmi = +enteredWeight / (+enteredHeight * +enteredHeight); // here, the + operator converts the string values to numbers. This is a way to tell TypeScript that we know that the values are numbers. It prevents TypeScript from throwing an error when we try to perform a mathematical operation on the values.
-
-    console.log(bmi);
+    setCalculatedBMI(bmi);
   };
 
   const resetInputs = () => {
@@ -99,9 +102,17 @@ const App: React.FC = () => {
               </IonButton>
             </IonCol>
           </IonRow>
-          <IonRow>
-            <IonCol></IonCol>
-          </IonRow>
+          {calculatedBMI && (
+            <IonRow>
+              <IonCol>
+                <IonCard>
+                  <IonCardContent>
+                    <h2>{calculatedBMI}</h2>
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+          )}
         </IonGrid>
       </IonContent>
     </IonApp>
@@ -117,3 +128,5 @@ export default App;
 // 1. Import useRef: You've already imported useRef in your code with import React, { useRef } from 'react';.
 // 2. Create a Ref Object: const myRef = useRef(initialValue) or const weightInputRef = useRef<HTMLIonInputElement>(null); => initialValue (optional): You can provide an initial value if you want. This is particularly useful for initializing the ref to a DOM element, as in the case of useRef(null).
 // 3. Accessing the Current Value: You can access the current value of the ref using myRef.current. This is a property of the ref object that holds the current value. You can also assign new values to it.
+
+// {calculatedBMI && ( <IonRow> ... </IonRow> /// here we use the && operator to conditionally render the card. The && operator is a way to conditionally render elements in React. It's a way to say that if the condition is true, render the element. If the condition is false, don't render the element. In this case, we're saying that if calculatedBMI is truthy, render the card. If calculatedBMI is falsy, don't render the card. This is a way to prevent the card from being rendered when the user hasn't calculated the BMI yet.

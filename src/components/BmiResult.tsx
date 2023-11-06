fb3fbccfe92884db3329010adc784b73e2456b22
leila <leila.zitouni55@gmail.com>
2023-11-06 09:20:47 +0100

    added checks: tupe number to input and checks to enteredHeight and enteredWeight

--- src/components/BmiResult.tsx
+++ src/components/BmiResult.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { IonRow, IonCol, IonCard, IonCardContent } from '@ionic/react';
 
-const BmiResult: React.FC<{ result: number | string }> = (props) => {
-  // here we create a new component called BmiResult. It's a functional component that takes one prop: result. The result prop is a number or a string. We specify the type of the prop using the React.FC type. The React.FC type is a generic type that takes an object as an argument. The object specifies the type of the prop. In this case, the object has one property: result. The result prop is a number or a string. The React.FC type returns a React component.
+const BmiResult: React.FC<{ result: number }> = (props) => {
+  // here we create a new component called BmiResult. It's a functional component that takes one prop: result. The result prop is a number. We specify the type of the prop using the React.FC type. The React.FC type is a generic type that takes an object as an argument. The object specifies the type of the prop. In this case, the object has one property: result. The result prop is a number. The React.FC type returns a React component.
   return (
     <IonRow>
       <IonCol>
         <IonCard>
-          <IonCardContent>
-            <h2>{props.result}</h2>
+          <IonCardContent className="ion-text-center">
+            <h2>Your Body-Mass-Index</h2>
+            <h3>{props.result.toFixed(2)}</h3>
           </IonCardContent>
         </IonCard>
       </IonCol>
